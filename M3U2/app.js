5b2980680dfcbfd626656ffce72e018fb81dbb4f
denisperaltac <denisperaltac@gmail.com>
2022-04-08 12:31:58 -0300

    Vamoooo

--- M3U2/app.js
+++ M3U2/app.js
@@ -5,13 +5,17 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/historia');
+var nosotrosRouter = require('./routes/nosotros')
+var novedadesRouter = require('./routes/novedades')
+var visitanosRouter = require('./routes/visitanos')
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +24,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/historia', usersRouter);
+app.use('/nosotros', nosotrosRouter);
+app.use('/novedades', novedadesRouter);
+app.use('/visitanos', visitanosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
