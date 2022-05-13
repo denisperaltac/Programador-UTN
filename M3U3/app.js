840635129bdb82864d731582eccfeded8932e4c5
denisperaltac <denisperaltac@gmail.com>
2022-05-13 17:34:43 -0300

    vamo

--- M3U3/app.js
+++ M3U3/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 require('dotenv').config();
 
+var pool = require('./models/db');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -27,6 +29,54 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+//Manejo Base de Datos
+
+// INSERT
+
+// var obj = {
+//   name: 'Denis',
+//   last_name: 'Peralta',
+//   email: 'denisperaltac@gmail.com',
+//   password: 'Caffalac12345'
+// }
+
+// pool.query('insert into users set ?', [obj]).then(function (resultados) {
+//   console.log(resultados)
+// })
+
+// var obj = {
+//   name: 'Lionel',
+//   last_name: 'Messi',
+//   email: 'lioMessi@gmail.com',
+//   password: 'Barcelona2019'
+// }
+
+// pool.query('insert into users set ?', [obj]).then(function (resultados) {
+//   console.log(resultados)
+// })
+
+//Como guarde dos veces se me crearon dos Messi
+
+// DELETE
+
+// var id = 2;
+
+// pool.query('delete from users where id_users=?', [id]).then(function(resultados) {
+//   console.log(resultados);
+// })
+
+// UPDATE
+
+// var id = 3;
+// var obj = {
+//   id_users: 2
+// }
+
+// pool.query('update users set ? where id_users=?', [obj, id]).then(function (resultados) {
+//   console.log(resultados);
+// })
+
 
 
 // catch 404 and forward to error handler
