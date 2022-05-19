f03f01b26188342c84d4eae19d570406ebd5c3a4
denisperaltac <denisperaltac@gmail.com>
2022-05-19 11:20:48 -0300

    h

--- M3U3 (Principal)/app.js
+++ M3U3 (Principal)/app.js
@@ -1,43 +1,59 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-require('dotenv').config();
+require("dotenv").config();
 
-var pool = require('./models/db');
+var pool = require("./models/db");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var loginRouter = require("./routes/admin/login");
 
 var app = express();
-const hbs = require('hbs');
+const hbs = require("hbs");
 
 //this required before view engine setup
-hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerPartials(__dirname + "/views/partials");
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/admin/login", loginRouter);
+
+// secured = async (req, res, next) => {
+//   try {
+//     console.log(req.session.id_usuario);
+//     if (req.session.id_usuario) {
+//       next();
+//     } else {
+//       res.redirect("/admin/login");
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// app.use("/admin/novedades", secured, adminNovedadesRouter);
 
 //Manejo Base de Datos
 
 // SELECT
 
-pool.query('select * from users').then(function (resultados) {
-  console.log(resultados)
+pool.query("select * from usuarios").then(function (resultados) {
+  console.log(resultados);
 });
-
 
 // INSERT
 
@@ -84,22 +100,20 @@ pool.query('select * from users').then(function (resultados) {
 //   console.log(resultados);
 // })
 
-
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- M3U3 (Principal)/routes/index.js
+++ M3U3 (Principal)/routes/index.js
@@ -1,47 +1,56 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var nodemailer = require('nodemailer');
-
+var nodemailer = require("nodemailer");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Caffito' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Caffito" });
 });
 
-router.post('/', async(req, res, next) => {
+router.post("/", async (req, res, next) => {
   var nombre = req.body.nombre;
   var apellido = req.body.apellido;
   var email = req.body.email;
   // if (req.body.telefono != '') {
   // var tel = ' y su número de teléfono ' + req.body.tel + '. ';
   // }
-  
-  if(req.body.tel != '') {
-    var tel = '<br>Teléfono: ' + req.body.tel;
-  } else {var tel = '.'}
+
+  if (req.body.tel != "") {
+    var tel = "<br>Teléfono: " + req.body.tel;
+  } else {
+    var tel = ".";
+  }
   var mensaje = req.body.mensaje;
 
   var obj = {
-    to: 'denisperaltac@gmail.com',
-    subject: 'CONTACTO DESDE LA WEB CAFFITO',
-    html: nombre + ' ' + apellido + ' se contacto a través de la web.' + '<br>Email: ' + email + tel + '<br>Dejo el siguiente mensaje: ' + mensaje
-  }
+    to: "denisperaltac@gmail.com",
+    subject: "CONTACTO DESDE LA WEB CAFFITO",
+    html:
+      nombre +
+      " " +
+      apellido +
+      " se contacto a través de la web." +
+      "<br>Email: " +
+      email +
+      tel +
+      "<br>Dejo el siguiente mensaje: " +
+      mensaje,
+  };
 
   var transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
     auth: {
       user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS 
-    }
-  })
+      pass: process.env.SMTP_PASS,
+    },
+  });
 
   var info = await transporter.sendMail(obj);
 
   res.render("index", {
     message: "Mensaje enviado correctamente",
   });
-  
 });
-  
+
 module.exports = router;
