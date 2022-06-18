dfb9b0cf786819effd9151ec6a4dc00890b02fe0
Denis Peralta <denisperaltac@gmail.com>
2022-06-18 11:23:44 -0300

    h

--- M3U3 (Principal)/caffalac.sql
+++ M3U3 (Principal)/caffalac.sql
@@ -2,8 +2,8 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: :3306
--- Generation Time: Jun 09, 2022 at 06:21 PM
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jun 18, 2022 at 02:22 PM
 -- Server version: 5.7.36
 -- PHP Version: 7.4.26
 
@@ -18,8 +18,174 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
+-- Database: `academia`
+--
+CREATE DATABASE IF NOT EXISTS `academia` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+USE `academia`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `academias`
+--
+
+DROP TABLE IF EXISTS `academias`;
+CREATE TABLE IF NOT EXISTS `academias` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) COLLATE utf8_bin NOT NULL,
+  `teléfono` varchar(45) COLLATE utf8_bin NOT NULL,
+  `web` varchar(100) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `alumnos`
+--
+
+DROP TABLE IF EXISTS `alumnos`;
+CREATE TABLE IF NOT EXISTS `alumnos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(80) COLLATE utf8_bin NOT NULL,
+  `apellido` varchar(80) COLLATE utf8_bin NOT NULL,
+  `email` varchar(255) COLLATE utf8_bin NOT NULL,
+  `teléfono` int(40) NOT NULL,
+  `academia_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `alumno_fk` (`academia_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `alumnos_x_cursos`
+--
+
+DROP TABLE IF EXISTS `alumnos_x_cursos`;
+CREATE TABLE IF NOT EXISTS `alumnos_x_cursos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `alumno_id` int(11) NOT NULL,
+  `curso_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `alumno_id_fk` (`alumno_id`),
+  KEY `curso_id_fk` (`curso_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cursos`
+--
+
+DROP TABLE IF EXISTS `cursos`;
+CREATE TABLE IF NOT EXISTS `cursos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(80) COLLATE utf8_bin NOT NULL,
+  `descripción` text COLLATE utf8_bin NOT NULL,
+  `profesor_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `cursos_profesorid_fk` (`profesor_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notas`
+--
+
+DROP TABLE IF EXISTS `notas`;
+CREATE TABLE IF NOT EXISTS `notas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nota` int(11) NOT NULL,
+  `alumno_id` int(11) NOT NULL,
+  `curso_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `notas_alumnoid_fk` (`alumno_id`),
+  KEY `notas_cursoid_fk` (`curso_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profesores`
+--
+
+DROP TABLE IF EXISTS `profesores`;
+CREATE TABLE IF NOT EXISTS `profesores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(80) COLLATE utf8_bin NOT NULL,
+  `apellido` varchar(80) COLLATE utf8_bin NOT NULL,
+  `teléfono` varchar(45) COLLATE utf8_bin NOT NULL,
+  `email` varchar(255) COLLATE utf8_bin NOT NULL,
+  `academia_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `profesor_academiaid_fk` (`academia_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(80) COLLATE utf8_bin NOT NULL,
+  `apellido` varchar(80) COLLATE utf8_bin NOT NULL,
+  `fecha_nacimiento` datetime DEFAULT NULL,
+  `TELEFONO` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `fecha_nacimiento`, `TELEFONO`) VALUES
+(1, 'pelu', 'peralta', '1996-07-16 04:00:00', '3408679045');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `alumnos`
+--
+ALTER TABLE `alumnos`
+  ADD CONSTRAINT `alumno_fk` FOREIGN KEY (`academia_id`) REFERENCES `academias` (`id`);
+
+--
+-- Constraints for table `alumnos_x_cursos`
+--
+ALTER TABLE `alumnos_x_cursos`
+  ADD CONSTRAINT `alumno_id_fk` FOREIGN KEY (`alumno_id`) REFERENCES `alumnos` (`id`),
+  ADD CONSTRAINT `curso_id_fk` FOREIGN KEY (`curso_id`) REFERENCES `cursos` (`id`);
+
+--
+-- Constraints for table `cursos`
+--
+ALTER TABLE `cursos`
+  ADD CONSTRAINT `cursos_profesorid_fk` FOREIGN KEY (`profesor_id`) REFERENCES `profesores` (`id`);
+
+--
+-- Constraints for table `notas`
+--
+ALTER TABLE `notas`
+  ADD CONSTRAINT `notas_alumnoid_fk` FOREIGN KEY (`alumno_id`) REFERENCES `alumnos` (`id`),
+  ADD CONSTRAINT `notas_cursoid_fk` FOREIGN KEY (`curso_id`) REFERENCES `cursos` (`id`);
+
+--
+-- Constraints for table `profesores`
+--
+ALTER TABLE `profesores`
+  ADD CONSTRAINT `profesor_academiaid_fk` FOREIGN KEY (`academia_id`) REFERENCES `academias` (`id`);
+--
 -- Database: `caffalac`
 --
+CREATE DATABASE IF NOT EXISTS `caffalac` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `caffalac`;
 
 -- --------------------------------------------------------
 
@@ -35,17 +201,17 @@ CREATE TABLE IF NOT EXISTS `novedades` (
   `cuerpo` text COLLATE utf8_unicode_ci NOT NULL,
   `img_id` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `novedades`
 --
 
 INSERT INTO `novedades` (`id`, `titulo`, `subtitulo`, `cuerpo`, `img_id`) VALUES
-(6, 'Noticia 1', 'lorem lorem lorem', 'lorem lorem lorem lorem lorem lorem lorem', NULL),
+(6, 'Noticia 1', 'lorem lorem lorem', 'lorem lorem lorem lorem lorem lorem lorem', 'jpst31fglzorffjzl05s'),
 (13, 'Noticia 2', ' Lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem', ' Lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem', 's2qgqyuycgpk5xerld4e'),
-(14, 'Noticia 4', ' Lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem', 'Hol Lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem Lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem', ''),
-(15, 'Noticia 5', ' Lorem lorem lorem lorem lorem lorem lorem', ' Lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem ', '');
+(18, 'Noticia 3', 'Lorem', 'Lorem Lorem Lorem', 'l02p9cfsrfyccvo8tsd8'),
+(14, 'Noticia 4', ' Lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem', 'Hol Lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem Lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem', '');
 
 -- --------------------------------------------------------
 
@@ -69,6 +235,85 @@ INSERT INTO `usuarios` (`id`, `usuario`, `password`) VALUES
 (1, '', 'd41d8cd98f00b204e9800998ecf8427e'),
 (2, 'Denis', '81dc9bdb52d04dc20036dbd8313ed055'),
 (3, 'Pelu', '81dc9bdb52d04dc20036dbd8313ed055');
+--
+-- Database: `crud`
+--
+CREATE DATABASE IF NOT EXISTS `crud` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+USE `crud`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `empleados`
+--
+
+DROP TABLE IF EXISTS `empleados`;
+CREATE TABLE IF NOT EXISTS `empleados` (
+  `id_emp` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(40) COLLATE utf8_bin NOT NULL,
+  `apellido` varchar(40) COLLATE utf8_bin NOT NULL,
+  `ocupacion` varchar(40) COLLATE utf8_bin NOT NULL,
+  `edad` int(3) NOT NULL,
+  `salario` int(10) NOT NULL,
+  `email` varchar(320) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id_emp`)
+) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `empleados`
+--
+
+INSERT INTO `empleados` (`id_emp`, `nombre`, `apellido`, `ocupacion`, `edad`, `salario`, `email`) VALUES
+(1, 'Juan', 'Hagan', 'Programador Senior', 32, 120000, 'juan_hagan@bignet.com'),
+(2, 'Gonzalo', 'Pillai', 'Programador Senior', 32, 110000, 'g_pillai@bignet.com'),
+(3, 'Ana', 'Dharma', 'Desarrollador Web', 27, 90000, 'ana@bignet.com'),
+(4, 'Maria', 'Anchor', 'Desarrollador Web', 26, 85000, 'mary@bignet.com'),
+(5, 'Alfredo', 'Fernandez', 'Programador', 31, 75000, 'af@bignet.com'),
+(6, 'Juan', 'Aguero', 'Programador', 34, 80000, 'juan@bignet.com'),
+(7, 'Eduardo', 'Sacan', 'Programador', 25, 75000, 'eddie@bignet.com'),
+(8, 'Alejandro', 'Nanda', 'Programador', 32, 70000, 'alenanda@bignet.com'),
+(10, 'Paublo', 'Simon', 'Especialista Multimedia', 43, 85000, 'ps@bignet.com'),
+(11, 'Arturo', 'Hernandez', 'Especialista Multimedia', 32, 75000, 'arturo@bignet.com'),
+(12, 'Jimena', 'Cazado', 'Diseñador Web Senior', 32, 110000, 'jimena@bignet.com'),
+(13, 'Roberto', 'Luis', 'Administrador de Sistemas', 35, 100000, 'roberto@bignet.com'),
+(14, 'Daniel', 'Gutierrez', 'Administrador de Sistemas', 34, 90000, 'daniel@bignet.com'),
+(15, 'Miguel', 'Harper', 'Ejecutivo de Ventas Senior', 36, 120000, 'miguel@bignet.com'),
+(16, 'Monica', 'Sanchez', 'Ejecutivo de Ventas', 30, 90000, 'monica@bignet.com'),
+(17, 'Alicia', 'Simlai', 'Ejecutivo de Ventas', 27, 70000, 'alicia@bignet.com'),
+(18, 'Jose', 'Iriarte', 'Ejecutivo de Ventas', 27, 72000, 'jose@bignet.com'),
+(19, 'Sabrina', 'Allende', 'Gerente de Soporte Tecnico', 32, 200000, 'sabrina@bignet.com'),
+(20, 'Pedro', 'Campion', 'Gerente de Finanzas', 36, 220000, 'pedro@bignet.com'),
+(21, 'Mariano', 'Dharma', 'Presidente', 28, 300000, 'mariano@bignet.com'),
+(22, 'Francisco', 'Perez', 'Programador', 26, 90000, 'francisco@bignet.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jugadores`
+--
+
+DROP TABLE IF EXISTS `jugadores`;
+CREATE TABLE IF NOT EXISTS `jugadores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) COLLATE utf8_bin NOT NULL,
+  `equipo` varchar(50) COLLATE utf8_bin NOT NULL,
+  `nacionalidad` varchar(50) COLLATE utf8_bin NOT NULL,
+  `estado` varchar(20) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `jugadores`
+--
+
+INSERT INTO `jugadores` (`id`, `nombre`, `equipo`, `nacionalidad`, `estado`) VALUES
+(1, 'Lionel Messi', 'PSG', 'Argentina', 'Jugador Activo'),
+(2, 'Cristiano Ronaldo', 'Manchester United', 'Portugal', 'Jugador Activo'),
+(6, 'Pep Guardiola', 'Manchester City', 'Español', 'DT'),
+(7, 'Marcelo Gallardo', 'River Plate', 'Argentina', 'DT'),
+(8, 'Jürgen Klopp', 'Liverpool', 'Aleman', 'DT'),
+(9, 'Ronaldo Nazario', '-', 'Brasil', 'Retirado'),
+(11, 'Gabinho', 'The shararas remix', 'Argentina', 'Jugador Activo');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
